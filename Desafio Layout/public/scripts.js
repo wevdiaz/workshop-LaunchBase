5b4f732c47f751c0c971e7184ad4447357e47e11
Weverson Diazz <wevers.info@gmail.com>
2020-05-20 22:37:46 -0300

    Menu Ativo
    Deixando o menu ativo conforme a página selecionada.

--- Desafio Layout/public/scripts.js
+++ Desafio Layout/public/scripts.js
@@ -1,17 +1,11 @@
-const modalOverlay = document.querySelector(".modal-overlay");
+const currentPage = location.pathname;
+const menuItems = document.querySelectorAll("header .links a");
 
-const cards = document.querySelectorAll(".card");
-
-for (let card of cards) {
-    card.addEventListener("click", function(){
-        const videoId = card.getAttribute("id");
-
-        //direcionar para página especifíca
-        window.location.href = `/video?id=${ videoId }`;
-        
-        
-
-    });
+for ( item of menuItems) {
+    if (currentPage.includes(item.getAttribute("href"))) {
+        item.classList.add("active");
+    }
 }
+
 
 
