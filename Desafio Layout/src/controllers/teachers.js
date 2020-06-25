b814a6ce18bd519629ca7165c46b6ffef0a7b058
Weverson Diazz <63312710+wevdiaz@users.noreply.github.com>
2020-06-24 21:40:03 -0300

    add teachers.js

---
+++ Desafio Layout/src/controllers/teachers.js
@@ -0,0 +1,211 @@
+const fs = require("fs");
+const dado = require("../dados.json");
+const { encontrarIdade, encontrarData } = require("../utils");
+const intl = require("intl");
+
+
+// index
+exports.index = function(req, res){
+
+    const teachers = dado.teachers.map(function(teacher){
+        const teacherMaterias = {
+            ...teacher,
+            materias: teacher.materias.split(",")
+        }
+
+        return teacherMaterias;
+    });
+
+
+    return res.render("teachers/index", { teachers });
+}
+
+// create
+exports.create = function(req, res){
+    return res.render("teachers/create");
+}
+
+
+// post 
+
+exports.post = function(req, res) {
+    
+    const keys = Object.keys(req.body);
+
+    for (key of keys) {
+        
+        if (req.body[key] == "") {
+            return res.send("Campo em branco, preencha!");
+        }
+    }
+     
+
+    req.body.nascimento = Date.parse(req.body.nascimento);
+    req.body.created_at = Date.now();
+    req.body.id = Number(dado.teachers.length + 1);
+
+    const { id, avatar_url, nome, nascimento, formacao, modalidade, materias, created_at } = req.body;
+    
+    dado.teachers.push(
+        {id,
+        avatar_url,
+        nome,
+        nascimento,
+        formacao,
+        modalidade,
+        materias,
+        created_at}
+    );
+
+
+    fs.writeFile("dados.json", JSON.stringify(dado, null, 2), function(err){
+        if (err) return res.send("write file error!");
+
+        return res.redirect("/teachers");
+    });
+
+     //return res.send(req.body);
+}
+
+
+// show
+
+exports.show = function(req, res) {
+    
+    const { id } = req.params;
+
+    const foundTeacher = dado.teachers.find(function(teacher){
+        return teacher.id == id;
+    });
+
+    if (!foundTeacher) {
+        return res.send("Teacher not found!");
+    }
+    
+    const teacher = {
+        ...foundTeacher,
+        idade: encontrarIdade(foundTeacher.nascimento),
+        formacao:escolhaDaFormacao(foundTeacher.formacao),
+        modalidade: escolherModalidade(foundTeacher.modalidade),
+        materias: foundTeacher.materias.split(","),
+        created_at: new intl.DateTimeFormat("pt-BR").format(foundTeacher.created_at)
+    }
+
+    return res.render("teachers/show", { teacher });
+}
+
+
+// edit
+
+exports.edit =  function(req, res){
+
+    const { id } = req.params;
+
+    const foundTeacher = dado.teachers.find(function(teacher){
+        return teacher.id == id;
+    });
+
+    if (!foundTeacher) {
+        return res.send("Teacher not found!");
+    }
+
+    const teacher = {
+        ...foundTeacher,
+        nascimento: encontrarData(foundTeacher.nascimento).iso
+    }
+
+    
+
+    return res.render("teachers/edit", { teacher });
+}
+
+
+// put
+exports.put = function(req, res){
+
+    const { id } = req.body;
+    let index = 0;
+
+    const foundTeacher = dado.teachers.find(function(teacher, foundIndex){
+        if (id == teacher.id) {
+            index = foundIndex
+            return true;
+        }
+    });
+
+    if (!foundTeacher) return res.send("Teacher not found!");
+
+    const teacher = {
+        ...foundTeacher,
+        ... req.body,
+        nascimento: Date.parse(req.body.nascimento),
+        id: Number(req.body.id)        
+    }
+
+    dado.teachers[index] = teacher;
+
+    fs.writeFile("dados.json", JSON.stringify(dado, null, 2), function(err){
+        if (err)  return res.send("write error!");
+
+        return res.redirect(`/teachers/${id}`);
+    });
+}
+
+// delete
+exports.delete = function(req, res) {
+    const { id } = req.body;
+
+    const filteredTeachers = dado.teachers.filter(function(teacher){
+        return teacher.id != id
+    });
+
+    dado.teachers = filteredTeachers;
+
+    fs.writeFile("dados.json", JSON.stringify(dado, null, 2), function(err){
+        if(err) return res.send("Write file error!");
+
+        return res.redirect("/teachers");
+
+    });
+
+}
+
+
+
+// ajuste form => Modalidade
+function escolherModalidade(opcao){
+    
+    if(opcao == "presencial"){
+        return "Presencial"
+    }
+    else {
+        return "A distânca"
+    }
+}
+
+
+// ajuste form => Formação
+function escolhaDaFormacao(escolha){
+
+    switch(escolha) {
+        
+        case "medio":
+           return escolha = "Ensino Médio Completo";
+            
+
+
+        case "superior":
+           return escolha = "Ensino Superior Completo";
+            
+
+
+        case "mestrado":
+           return escolha = "Mestrado";
+            
+
+
+        case "doutorado":
+           return escolha = "Doutorado";
+            
+    }
+}
