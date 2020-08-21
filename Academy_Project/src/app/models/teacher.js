b2b826941d6da558762613701660abed85fe6347
Weverson Diazz <wevers.info@gmail.com>
2020-08-20 22:10:21 -0300

    subqueries in paginate
    Pegando total de registro usando subqueries.

--- Academy_Project/src/app/models/teacher.js
+++ Academy_Project/src/app/models/teacher.js
@@ -118,23 +118,30 @@ module.exports = {
     paginate(params) {
         const { filter, limit, offset, callback } = params;
 
-        let query = `
-        SELECT teachers.*, count(students) as total_students
-        FROM teachers
-        LEFT JOIN students ON (teachers.id = students.teacher_id)
-        `;
+        let query = "",
+            filterQuery = "",
+            totalQuery = `(
+                SELECT count(*) FROM teachers
+            ) AS total`
 
         if ( filter ) {
-            query = `${query}
+            filterQuery = `
             WHERE teachers.name ILIKE '%${filter}%'
             OR teachers.subjects_taught ILIKE '%${filter}%'
             `
         }
 
-        query = `${query}        
-        GROUP BY teachers.id
-        ORDER BY name ASC
-        LIMIT $1 OFFSET $2
+        totalQuery = `(
+            SELECT count(*) FROM teachers
+            ${filterQuery}
+        ) AS total`;
+
+        query = `
+        SELECT teachers.*, ${totalQuery}, count(students) AS total_students
+        FROM teachers
+        LEFT JOIN students ON (teachers.id = students.teacher_id)
+        ${filterQuery}
+        GROUP BY teachers.id LIMIT $1 OFFSET $2
         `
 
         db.query(query, [limit, offset], function(err, results){
