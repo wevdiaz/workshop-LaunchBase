c39667e7ba0e4a3feb7f46a7c6aabec383472506
Weverson Diazz <wevers.info@gmail.com>
2020-12-12 16:36:26 -0300

    função checando campos do jogo

--- atividades javascript/jogo_da_velha/scripts.js
+++ atividades javascript/jogo_da_velha/scripts.js
@@ -46,9 +46,44 @@ function beginSpaces() {
                     playTime = player1;
                 }
                 updateMostrador();
+                checkWinner();
             }
 
         });
     }
     
+}
+
+
+function checkWinner() {
+
+    let a1 = document.getElementById("a1").getAttribute("jogada");
+    let a2 = document.getElementById("a2").getAttribute("jogada");
+    let a3 = document.getElementById("a3").getAttribute("jogada");
+
+    let b1 = document.getElementById("b1").getAttribute("jogada");
+    let b2 = document.getElementById("b2").getAttribute("jogada");
+    let b3 = document.getElementById("b3").getAttribute("jogada");
+
+    let c1 = document.getElementById("c1").getAttribute("jogada");
+    let c2 = document.getElementById("c2").getAttribute("jogada");
+    let c3 = document.getElementById("c3").getAttribute("jogada");
+
+    let vencedor = "";
+
+    if ((a1 == b1 && a1 == c1 && a1 != "") || (a1 == a2 && a1 == a3 && a1 != "") || (a1 == b2 && a1 == c3 && a1 != "")) {
+        vencedor = a1;
+    }
+    else if ((b2 == b1 && b2 == b3 && b2 != "") || (b2 == a2 && b2 == c2 && b2 != "") || (b2 == a3 && b2 ==c1 && b2 != "")) {
+        vencedor = b2;
+    }
+    else if (((c3 == c2 && c3 == c1) || (c3 == a3 && c3 == b3)) && c3 != "") {
+        vencedor = c3;
+    }
+
+    if (vencedor != "") {
+        gameOver = true;
+
+        alert(`O vencedor foi o '${vencedor}'`);
+    }
 }
